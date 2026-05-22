e8703cfeaec228573006467779c2a9873fa0c5fc
as1028392 <as1028392@gmail.com>
2026-05-22 11:47:44 +0300

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -3,11 +3,13 @@ export default {
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
   testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
   collectCoverageFrom: [
     'src/**/*.ts',
     '!src/**/*.d.ts',
-    '!src/server.ts',
+    '!src/index.ts',
   ],
+  coverageDirectory: 'coverage',
   coverageThreshold: {
     global: {
       branches: 70,
